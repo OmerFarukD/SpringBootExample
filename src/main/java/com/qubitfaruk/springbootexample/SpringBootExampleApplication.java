9a8b7b71d447c62da03ae399ec5f84a04c4d57ce
Omer Faruk Dogan <omer.crlys.44@gmail.com>
2022-04-12 22:33:04 +0300

    Config

--- src/main/java/com/qubitfaruk/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/com/qubitfaruk/springbootexample/SpringBootExampleApplication.java
@@ -3,11 +3,15 @@ package com.qubitfaruk.springbootexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 public class SpringBootExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootExampleApplication.class, args);
+
     }
 
 }
