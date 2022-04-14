86d5d786530a856c03ed651ca1a815b0e8c5b047
Omer Faruk Dogan <omer.crlys.44@gmail.com>
2022-04-15 01:38:38 +0300

    model,object

--- src/main/java/com/qubitfaruk/springbootexample/controller/TymeleafController.java
+++ src/main/java/com/qubitfaruk/springbootexample/controller/TymeleafController.java
@@ -1,8 +1,12 @@
 package com.qubitfaruk.springbootexample.controller;
 
+import com.qubitfaruk.springbootexample.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TymeleafController {
@@ -24,5 +28,34 @@ public class TymeleafController {
         model.addAttribute("key_model1","Ben modelden geldim=>1.");
         model.addAttribute("key_model2","Ben modelden geldim=>2.");
         return "thymeleaf_file/thymeleaf3";
+    }
+    @GetMapping("/thymeleaf4")
+    public String getThymeleaf4Model(Model model){
+        model.addAttribute("key_model4","Ben modelden geldim=>1.");
+        return "thymeleaf4";
+    }
+    //http://localhost:8080/thymeleaf5
+    @GetMapping("/thymeleaf5")
+    public String getThymeleaf5Model(Model model){
+        model.addAttribute("key_model5","text");
+        ProductDto productDto=ProductDto.builder()
+                .productId(0L)
+                .productName("Elma")
+                .productPrice(30.0)
+                .build();
+        model.addAttribute("objectForProductDto",productDto);
+        return "thymeleaf5";
+    }
+    //http://localhost:8080/thymeleaf6
+    @GetMapping("/thymeleaf6")
+    public String getThymeleaf6Model(Model model){
+        model.addAttribute("key_model5","text");
+        List<ProductDto> myList=new ArrayList<ProductDto>();
+        myList.add(ProductDto.builder().productId(0L).productName("Elma").productPrice(30.0).build());
+        myList.add(ProductDto.builder().productId(1L).productName("Armut").productPrice(35.0).build());
+        myList.add(ProductDto.builder().productId(2L).productName("Kiraz").productPrice(33.0).build());
+        ProductDto productDto=ProductDto.builder().productId(0L).productName("Elma").productPrice(30.0).build();
+        model.addAttribute("ProductList",myList);
+        return "thymeleaf6";
     }
 }
---
+++ src/main/java/com/qubitfaruk/springbootexample/utils/TymeleafInformation.java
@@ -0,0 +1,7 @@
+package com.qubitfaruk.springbootexample.utils;
+
+public class TymeleafInformation {
+    // #{}=> messages.properties
+    //${} => Model
+    //@{} => css,js
+}
